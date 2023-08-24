2db9bf25c9b8d4e6af4a8f36336ca58ac382ebf7
lokeshk <lokesh.krishnamoorthy@intergy.com.au>
2023-08-24 23:44:52 +0530

    chromium location config

--- index.js
+++ index.js
@@ -42,15 +42,7 @@ app.post("/GraphGet", async (req, res) => {
 
 app.get("/convertTest", async (req,res)=>{
   try{
-    const browser = await puppeteer.launch({ headless: true, args:[
-      '--disable-gpu',
-      '--disable-dev-shm-usage',
-      '--disable-setuid-sandbox',
-      '--no-first-run',
-      '--no-sandbox',
-      '--no-zygote',
-      '--single-process',
-    ] });
+    const browser = await puppeteer.launch({ headless: true});
     // const browser = await puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io/' });
     const page = await browser.newPage();
 
@@ -68,7 +60,7 @@ app.get("/convertTest", async (req,res)=>{
       fullPage: true,
     });
 
-    // await browser.close();
+    await browser.close();
 
     res.set("Content-Type", "image/png");
     // res.send(imageBuffer);

573b5a95ef2164bdc9720bf23b1c4804f7d5641c
lokeshK12510 <lokesh.krishnamoorthy@intergy.com.au>
2023-08-24 19:11:51 +0530

    azure run check 6 puppeteer

--- index.js
+++ index.js
@@ -31,6 +31,63 @@ app.get("/GraphGet", async (req, res) => {
   }
 });
 
+app.post("/convertHtmlToImg", async (req, res) => {
+  try {
+    const browser = await puppeteer.launch({
+      headless: true,
+    });
+    const page = await browser.newPage();
+
+    // Set viewport width and height
+    await page.setViewport({ width: 1280, height: 720 });
+
+    let type = req.body.type;
+
+    async function findTemplate() {
+      switch (type) {
+        case "average":
+          return await fs.readFile("./templates/average.html", {
+            encoding: "utf8",
+          });
+        case "regulatory":
+          return await fs.readFile("./templates/regulatory.html", {
+            encoding: "utf8",
+          });
+
+        default:
+          return await fs.readFile("index.html", { encoding: "utf8" });
+      }
+    }
+
+    // Read the HTML content from the input HTML file
+    const htmlContent = await findTemplate();
+
+    await page.setContent(htmlContent);
+
+    // Navigate to a data URL with the HTML content
+    await page.goto(`data:text/html,${htmlContent}`, {
+      waitUntil: "networkidle0",
+    });
+
+    // const imageBuffer = await page.screenshot();
+    // Capture screenshot
+    const imageBuffer = await page.screenshot({
+      path: `images/chart${new Date().getTime()}.png`,
+      type: "png",
+      fullPage: true,
+    });
+
+    await browser.close();
+
+    res.set("Content-Type", "image/png");
+    // res.send(imageBuffer);
+    res.status(200).send(imageBuffer);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 // Start the server
 const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {

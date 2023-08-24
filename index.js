fc214ae7a769140876d648f8698b908f0236bc5f
lokeshK12510 <lokesh.krishnamoorthy@intergy.com.au>
2023-08-24 18:53:40 +0530

    azure run check

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
-const puppeteer = require("puppeteer");
-const fs = require("fs/promises");
+const express = require("express");
+const app = express();
 
-(async () => {
-  // Create a browser instance
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
+// Custom initialization logic before starting the server
+console.log("Initializing...");
 
-  // Create a new page
-  const page = await browser.newPage();
+// Define routes
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
 
-  // Set viewport width and height
-  await page.setViewport({ width: 1280, height: 720 });
-
-  //   const website_url = "https://www.intergy.com.au/";
-
-  //   // Open URL in current page
-  //   await page.goto(website_url, { waitUntil: "networkidle0" });
-  try {
-    // Read the HTML content from the input HTML file
-    const htmlContent = await fs.readFile("index.html", { encoding: "utf8" });
-
-    // Navigate to a data URL with the HTML content
-    await page.goto(`data:text/html,${htmlContent}`, {
-      waitUntil: "networkidle0",
-    });
-
-    // Capture screenshot
-    await page.screenshot({
-      path: "images/html.jpg",
-      type: "png",
-      fullPage: true,
-    });
-
-    console.log("Screenshot saved as html.png");
-  } catch (error) {
-    console.error("Error:", error);
-  } finally {
-    // Close the browser
-    await browser.close();
-  }
-})();
+// Start the server
+const PORT = process.env.PORT || 3002;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

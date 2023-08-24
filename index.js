7270fb5ca8f723edf82543e8c86e92c02cd4f1d9
lokeshK12510 <lokesh.krishnamoorthy@intergy.com.au>
2023-08-24 19:03:06 +0530

    azure run check 3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+// mock
+const data = require("./mock/GraphGet.json");
 
 // Custom initialization logic before starting the server
 console.log("Initializing...");
@@ -7,6 +9,15 @@ console.log("Initializing...");
 // Define routes
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+app.get("/GraphGet", async (req, res) => {
+  try {
+    res.status(200).send(data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // Start the server
